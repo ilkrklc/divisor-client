69a26a5440c6d17cae9437503ecfd17077b5d0a8
İlker Kılıç <ilkrklc@gmail.com>
2020-11-28 14:30:37 +0300

    Strip down boilerplate app parts & base page layout (#1)
    * Add prettier config and lint all codes
    
    * Add normalize.css as dependency
    
    * Format index.html
    
    * Remove boilerplate logo
    
    * Add git logo
    
    * Remove boilerplate component
    
    * Add color palette
    
    * Add base page layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <navbar />
+  <main>
+    <router-view />
+  </main>
+  <footer-bar />
 </template>
 
+<script>
+import { defineComponent } from 'vue';
+
+import Navbar from '@/components/Navbar.vue';
+import FooterBar from '@/components/FooterBar.vue';
+
+import 'normalize.css';
+
+export default defineComponent({
+  components: {
+    Navbar,
+    FooterBar,
+  },
+});
+</script>
+
 <style lang="scss">
+@use '@/styles/variables.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  box-sizing: border-box;
+  height: 100vh;
+  background-color: variables.$color-lighter;
+  color: variables.$color-text;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  main {
+    flex-grow: 1;
   }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="navbar">
+    <div id="navbar-name">
+      Divisor
+    </div>
+    <div class="navbar-links">
+      <router-link to="/" exact>Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <div id="navbar-github">
+      <a href="https://github.com/ilkrklc/divisor">
+        <img src="../assets/git-logo.png" alt="github project link" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'navbar',
+  components: {},
+});
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/variables.scss';
+
+#navbar {
+  display: flex;
+  justify-content: space-between;
+  height: 4rem;
+  max-height: 4rem;
+
+  #navbar-name {
+    width: 25%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: 700;
+    cursor: default;
+  }
+
+  .navbar-links {
+    display: flex;
+    justify-content: flex-start;
+    align-items: stretch;
+    flex-grow: 1;
+
+    a {
+      flex-grow: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-left: 0.5rem;
+      margin-right: 0.5rem;
+      font-weight: bold;
+      color: #2c3e50;
+      text-decoration: none;
+
+      &.router-link-exact-active {
+        color: variables.$color-danger;
+        text-decoration: underline;
+      }
+    }
+  }
+
+  #navbar-github {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-right: 1rem;
+      padding-left: 1rem;
+      height: 100%;
+
+      img {
+        width: 38px;
+        height: 38px;
+      }
+    }
+  }
+}
+</style>
