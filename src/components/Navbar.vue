92b0c5b1195625b3148309c4afb79d176e5e38fc
İlker Kılıç <ilkrklc@gmail.com>
2020-11-28 18:26:24 +0300

    Refactor navbar component
    - Change app name into a link
    - Add hover effect to git logo link
    - Rework styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,26 +1,46 @@
 <template>
   <div id="navbar">
-    <div id="navbar-name">
-      Divisor
-    </div>
+    <router-link id="navbar-name" to="/">Divisor</router-link>
     <div class="navbar-links">
-      <router-link to="/" exact>Home</router-link>
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
-    <div id="navbar-github">
-      <a href="https://github.com/ilkrklc/divisor">
+    <div
+      id="navbar-github"
+      :class="{ hovered: isGitLogoHovered }"
+      @mouseenter="handleGitLogoHover(true)"
+      @mouseleave="handleGitLogoHover(false)"
+    >
+      <a
+        href="https://github.com/ilkrklc/divisor"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         <img src="../assets/git-logo.png" alt="github project link" />
       </a>
+      <span class="navbar-github-description">
+        Visit project repository...
+      </span>
     </div>
   </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-  name: 'navbar',
-  components: {},
+  setup() {
+    const isGitLogoHovered = ref<boolean>(false);
+
+    function handleGitLogoHover(isHovered: boolean): void {
+      isGitLogoHovered.value = isHovered;
+    }
+
+    return {
+      isGitLogoHovered,
+      handleGitLogoHover,
+    };
+  },
 });
 </script>
 
@@ -30,60 +50,103 @@ export default defineComponent({
 #navbar {
   display: flex;
   justify-content: space-between;
-  height: 4rem;
-  max-height: 4rem;
+  height: 5rem;
+  max-height: 5rem;
 
   #navbar-name {
-    width: 25%;
+    width: 20rem;
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 1.5rem;
+    font-size: 1.6rem;
     font-weight: 700;
-    cursor: default;
+    color: variables.$color-medium;
+    margin-right: 2rem;
+    text-decoration: none;
   }
 
   .navbar-links {
     display: flex;
     justify-content: flex-start;
-    align-items: stretch;
+    align-items: center;
     flex-grow: 1;
 
     a {
-      flex-grow: 1;
       display: flex;
       justify-content: center;
       align-items: center;
-      margin-left: 0.5rem;
-      margin-right: 0.5rem;
+      font-size: 1.05rem;
       font-weight: bold;
-      color: #2c3e50;
+      color: rgba($color: variables.$color-text, $alpha: 0.75);
       text-decoration: none;
+      border-bottom-width: 0.75px;
+      border-bottom-style: solid;
+      border-bottom-color: transparent;
+      width: 6rem;
+      height: 2.75rem;
+      transition: color 0.1s ease-in, border-bottom-color 0.2s ease-in,
+        font-size 0.05ms ease-in;
+
+      &:not(:first-child) {
+        margin-left: 2rem;
+      }
 
       &.router-link-exact-active {
-        color: variables.$color-danger;
-        text-decoration: underline;
+        font-size: 1.1rem;
+        color: variables.$color-medium;
+        border-bottom-color: rgba($color: variables.$color-medium, $alpha: 0.5);
       }
     }
   }
 
   #navbar-github {
+    position: relative;
     display: flex;
-    justify-content: center;
+    justify-content: flex-end;
     align-items: center;
+    width: 15rem;
+    margin-right: 1.5rem;
+    overflow: hidden;
+
+    &.hovered {
+      .navbar-github-description {
+        right: 45px;
+      }
+
+      a img {
+        opacity: 1;
+      }
+    }
 
     a {
       display: flex;
       justify-content: center;
       align-items: center;
-      padding-right: 1rem;
-      padding-left: 1rem;
       height: 100%;
 
       img {
         width: 38px;
         height: 38px;
+        z-index: 20;
+        background-color: variables.$color-lighter;
+        opacity: 0.75;
+        transition: opacity 0.1s ease-in-out;
       }
+    }
+
+    .navbar-github-description {
+      position: absolute;
+      right: -10rem;
+      top: 0;
+      height: 100%;
+      width: 10rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: right 0.25s ease-in-out;
+      z-index: 1;
+      font-size: 0.8rem;
+      font-weight: 500;
     }
   }
 }
