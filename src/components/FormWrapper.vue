6215a6198347dac6369085b8ced58495f7399c78
İlker Kılıç <ilkrklc@gmail.com>
2020-11-29 23:38:50 +0300

    Revise form input styles

--- src/components/FormWrapper.vue
+++ src/components/FormWrapper.vue
@@ -23,12 +23,13 @@ export default defineComponent({
 .form {
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  justify-content: space-evenly;
   align-items: stretch;
   flex-grow: 1;
   padding: 1rem 5rem;
 
   input[type='text'] {
+    height: 1.25rem;
     padding-top: 0.5rem;
     padding-bottom: 0.5rem;
     padding-left: 0.75rem;
@@ -46,6 +47,11 @@ export default defineComponent({
 
   select {
     flex-grow: 1;
+    height: 2.5rem;
+    padding-top: 0.25rem;
+    padding-bottom: 0.25rem;
+    padding-left: 0.75rem;
+    padding-right: 0.75rem;
   }
 
   select,
@@ -69,12 +75,20 @@ export default defineComponent({
       align-items: center;
 
       input {
-        margin-right: 0.25rem;
+        height: 1rem;
+        width: 1rem;
+        margin-right: 0.5rem;
       }
     }
 
     &:not(.checkbox-label) span {
       margin-bottom: 0.5rem;
+    }
+
+    span {
+      font-size: 0.9rem;
+      font-weight: 300;
+      align-self: flex-start;
     }
   }
 
