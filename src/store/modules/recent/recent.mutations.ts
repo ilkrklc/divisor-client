2c252a149dadaa9107dd12c2e4b2759a96baa3a9
İlker Kılıç <ilkrklc@gmail.com>
2020-11-29 22:40:49 +0300

    Add recent vuex module

---
+++ src/store/modules/recent/recent.mutations.ts
@@ -0,0 +1,27 @@
+import { MutationTree } from 'vuex';
+
+import { RecentState, RecentItem } from '@/store/modules/recent/recent.state';
+
+export enum RecentMutationType {
+  SetLoading = 'SET_LOADING',
+  SetItems = 'SET_ITEMS',
+  AddItem = 'ADD_ITEM',
+}
+
+export type RecentMutations = {
+  [RecentMutationType.SetLoading](state: RecentState, loading: boolean): void;
+  [RecentMutationType.SetItems](state: RecentState, items: RecentItem[]): void;
+  [RecentMutationType.AddItem](state: RecentState, item: RecentItem): void;
+};
+
+export const recentMutations: MutationTree<RecentState> & RecentMutations = {
+  [RecentMutationType.SetLoading](state, loading) {
+    state.loading = loading;
+  },
+  [RecentMutationType.SetItems](state, items) {
+    state.items = items;
+  },
+  [RecentMutationType.AddItem](state, item) {
+    state.items = [...state.items, ...[item]];
+  },
+};
