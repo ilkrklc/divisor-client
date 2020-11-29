b439b4fa1b751e40a7fe099e9756635a4d5e59ec
İlker Kılıç <ilkrklc@gmail.com>
2020-11-29 22:42:04 +0300

    Update store with created modules

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,15 @@
-import { createStore } from 'vuex';
+import { createStore, createLogger } from 'vuex';
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+import { State } from '@/store/state';
+import { modules, Modules } from '@/store/modules';
+
+import { RecentModule } from '@/store/modules/recent';
+import { GreetingModule } from '@/store/modules/greeting';
+
+export const store = createStore<State>({
+  plugins: process.env.NODE_ENV === 'development' ? [createLogger()] : [],
+  modules: modules,
 });
+
+export type Store = RecentModule<Pick<Modules, 'recent'>> &
+  GreetingModule<Pick<Modules, 'greeting'>>;
