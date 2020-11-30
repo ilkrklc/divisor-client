5f1a1d901ab1619853331df782d637fb6edc7cbe
İlker Kılıç <ilkrklc@gmail.com>
2020-11-30 12:48:28 +0300

    Change parse number helper parameter type

--- src/helpers/number.helpers.ts
+++ src/helpers/number.helpers.ts
@@ -3,9 +3,9 @@
  * @param {string} n Number input value as string
  * @returns {number | null} integer number if input value is correct null if not
  */
-export function parseNumber(n: string): number | null {
+export function parseNumber(n: string): number | undefined {
   const number = parseInt(n, 10);
-  if (isNaN(number)) return null;
+  if (isNaN(number)) return undefined;
 
   return number;
 }
