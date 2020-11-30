19e8be8f113f707c861128ca026feb60ed4a5207
İlker Kılıç <ilkrklc@gmail.com>
2020-11-30 12:39:54 +0300

    Fix property type logic for base result interface
    Add selectlist and recent item interfaces

--- src/typings/interfaces.ts
+++ src/typings/interfaces.ts
@@ -1,14 +1,31 @@
+import { CalculationType } from '@/typings/enums';
+
 export interface BaseResult {
-  divisors: number[];
+  divisors?: number[];
   count: number;
-  sum: number;
-  multiplication: number;
+  sum?: number;
+  multiplication?: number;
   greatest?: number;
   smallest?: number;
 }
 
 export interface DivisorFormState {
+  number?: number;
+  sort: 'not_defined' | 'asc' | 'desc';
+  onlyProperDivisors: boolean;
+}
+
+export interface CommonDivisorFormState {
   number: string;
   sort: 'not_defined' | 'asc' | 'desc';
   onlyProperDivisors: boolean;
+}
+
+export interface SelectList {
+  text: string;
+  value: string;
+}
+
+export interface RecentItem extends BaseResult {
+  calculationType: CalculationType;
 }
