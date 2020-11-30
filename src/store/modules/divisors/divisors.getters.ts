20aea3ab1e0b480c22ac8cfaa57af80189f7c1fe
İlker Kılıç <ilkrklc@gmail.com>
2020-11-30 12:46:37 +0300

    Remove result property from divisors state
    - Change set number action & mutation argument types
    - Remove redundant recent items interface

--- src/store/modules/divisors/divisors.getters.ts
+++ src/store/modules/divisors/divisors.getters.ts
@@ -2,14 +2,10 @@ import { GetterTree } from 'vuex';
 
 import { DivisorFormState } from '@/typings/interfaces';
 import { State } from '@/store/state';
-import {
-  DivisorsState,
-  DivisorsResult,
-} from '@/store/modules/divisors/divisors.state';
+import { DivisorsState } from '@/store/modules/divisors/divisors.state';
 
 export type DivisorsGetters = {
   divisorFormState(state: DivisorsState): DivisorFormState;
-  divisorResult(state: DivisorsState): DivisorsResult | undefined;
   divisorCalculating(state: DivisorsState): boolean;
 };
 
@@ -17,9 +13,6 @@ export const divisorsGetters: GetterTree<DivisorsState, State> &
   DivisorsGetters = {
   divisorFormState(state) {
     return state.form;
-  },
-  divisorResult(state) {
-    return state.result || undefined;
   },
   divisorCalculating(state) {
     return state.calculating;
--- src/store/modules/divisors/divisors.state.ts
+++ src/store/modules/divisors/divisors.state.ts
@@ -1,26 +1,15 @@
 import { DivisorFormState } from '@/typings/interfaces';
 
-export interface DivisorsResult {
-  divisors: number[];
-  count: number;
-  sum: number;
-  multiplication: number;
-  greatest: number;
-  smallest: number;
-}
-
 export type DivisorsState = {
   form: DivisorFormState;
   calculating: boolean;
-  result?: DivisorsResult;
 };
 
 export const divisorsState: DivisorsState = {
   form: {
-    number: '',
+    number: undefined,
     sort: 'not_defined',
     onlyProperDivisors: false,
   },
   calculating: false,
-  result: undefined,
 };
