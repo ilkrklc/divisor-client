74dff77f219948328e29a985d6c747e48b340536
İlker Kılıç <ilkrklc@gmail.com>
2020-11-30 15:02:21 +0300

    Divisors form updated with validators
    - Jsdoc comments added
    - Form, input namings updated

--- src/components/DivisorsForm.vue
+++ src/components/DivisorsForm.vue
@@ -1,17 +1,30 @@
 <template>
-  <form-wrapper :onSubmit="handleSubmit">
-    <label class="form-label">
+  <form-wrapper id="divisors-form" :onSubmit="handleSubmit">
+    <label class="form-label" for="number">
       <span>Type a natural number</span>
       <input
         type="text"
+        name="number"
+        id="number"
+        autocomplete="off"
         v-model="state.number"
-        @input="setNumber($event.target.value)"
+        @input="setNumber($event.target)"
         placeholder="Ex: 63"
       />
+      <small>{{
+        state.number === undefined
+          ? ValidationErrors.NumberEmpty
+          : ValidationErrors.NumberNotValid
+      }}</small>
     </label>
-    <label class="form-label">
+    <label class="form-label" for="sort">
       <span>You can sort output ordering</span>
-      <select v-model="state.sort" @change="setSort($event.target.value)">
+      <select
+        v-model="state.sort"
+        name="sort"
+        id="sort"
+        @change="setSort($event.target.value)"
+      >
         <option
           :key="sortOption.value"
           v-for="sortOption in sortOptions"
@@ -21,9 +34,11 @@
         </option>
       </select>
     </label>
-    <label class="form-label checkbox-label">
+    <label class="form-label checkbox-label" for="proper">
       <input
         type="checkbox"
+        name="proper"
+        id="proper"
         v-model="state.onlyProperDivisors"
         @change="setProperIndicator($event.target.checked)"
       />
@@ -36,61 +51,132 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { defineComponent, computed, ref } from 'vue';
 
 import { useStore } from '@/hooks/useStore';
-import { DivisorsActionTypes } from '@/store/modules/divisors/divisors.actions';
-
 import { useSortOptions } from '@/hooks/useSortOptions';
-import { parseNumber } from '@/helpers/number.helpers';
+import { DivisorsActionTypes } from '@/store/modules/divisors/divisors.actions';
+import { SortOptions, ValidationErrors } from '@/typings/enums';
+import {
+  validateSortList,
+  validateNumberInput,
+  toggleInputErrorText,
+  toggleFormErrorText,
+} from '@/helpers/validation.helpers';
 import DivisorResult from '@/models/divisor-result.model';
 
 import FormWrapper from '@/components/FormWrapper.vue';
-import { SortOptions } from '@/typings/enums';
 
 export default defineComponent({
   components: { FormWrapper },
   setup() {
+    /**
+     * Vuex store instance
+     */
     const store = useStore();
 
+    /**
+     * Holds a value to indicate if number input is touched until page load
+     */
+    const isNumberInputDirty = ref<boolean>(false);
+
+    /**
+     * Form state
+     */
     const state = computed(() => store.getters.divisorFormState);
 
+    /**
+     * Sort options as selectlist
+     */
     const sortOptions = useSortOptions();
 
-    function setNumber(value: string): void {
-      // get only numbers from input string
-      const numberText = value.replace(/\D/g, '');
+    /**
+     * Handles number input type event
+     * @param {HTMLInputElement} element Input element
+     */
+    function setNumber(element: HTMLInputElement): void {
+      // flag number input as dirty
+      if (isNumberInputDirty.value === false) isNumberInputDirty.value = true;
 
-      // parse input number
-      const number = parseNumber(numberText);
+      // destructure value
+      const { name, value } = element;
+
+      // validate number
+      const [result, number] = validateNumberInput(value);
+
+      // if validation fails find label element and flag with error class
+      toggleInputErrorText(result, name);
 
       // update state
       store.dispatch(DivisorsActionTypes.SetNumber, number);
     }
 
-    function setSort(value: 'asc' | 'desc'): void {
-      store.dispatch(DivisorsActionTypes.SetSort, value);
+    /**
+     * Handles sort select input change event
+     * @param {SortOptions} value Selected sort option
+     */
+    function setSort(value: SortOptions): void {
+      // initialize sort as local variable
+      let sort = value;
+
+      // validate sort expression
+      const validationResult = validateSortList(value);
+
+      // reset expression if not validated
+      if (validationResult === false) sort = SortOptions.NotDefined;
+
+      // update state
+      store.dispatch(DivisorsActionTypes.SetSort, sort);
     }
 
+    /**
+     * Handles only proper divisors checkbox change event
+     * @param {boolean} indicator Indicates proper divisor request
+     */
     function setProperIndicator(indicator: boolean): void {
+      // update state
       store.dispatch(DivisorsActionTypes.SetOnlyProperDivisors, indicator);
     }
 
+    /**
+     * Handles form submit event
+     */
     function handleSubmit(): void {
-      // destructure form state
-      const { number, sort: s, onlyProperDivisors: onlyProper } = state.value;
+      try {
+        // if no input is touched display number error
+        if (isNumberInputDirty.value === false) {
+          toggleInputErrorText(false, 'number');
 
-      // TODO - Display Error
-      if (!number) return;
+          return;
+        }
 
-      // try to get divisors
-      const result = new DivisorResult({
-        number,
-        sort: s === SortOptions.NotDefined ? undefined : s,
-        onlyProperDivisors: onlyProper,
-      });
+        // destructure form state
+        const { number, sort: s, onlyProperDivisors: onlyProper } = state.value;
+        // if no number found display number error
+        if (!number) {
+          toggleInputErrorText(false, 'number');
 
-      console.log({ result });
+          return;
+        }
+
+        // try to get divisors by initializing divisor result class using divisor package
+        const result = new DivisorResult({
+          number,
+          sort: s === SortOptions.NotDefined ? undefined : s,
+          onlyProperDivisors: onlyProper,
+        });
+
+        console.log({ result });
+
+        // reset form
+        store.dispatch(DivisorsActionTypes.SetNumber, undefined);
+        store.dispatch(DivisorsActionTypes.SetSort, SortOptions.NotDefined);
+        store.dispatch(DivisorsActionTypes.SetOnlyProperDivisors, false);
+        isNumberInputDirty.value = false;
+      } catch (error) {
+        // display common error text if unexpected error encountered
+        toggleFormErrorText(false, 'divisors-form');
+      }
     }
 
     return {
@@ -100,9 +186,8 @@ export default defineComponent({
       setSort,
       setProperIndicator,
       handleSubmit,
+      ValidationErrors,
     };
   },
 });
 </script>
-
-<style lang="scss" scoped></style>
