f88e95fbbf1527ce7e73e61cf25df623e89e2c62
İlker Kılıç <ilkrklc@gmail.com>
2020-12-03 14:04:17 +0300

    Fix gh-pages script errors (#19)

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,17 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# re index files
+# initialize a repo in dist folder
+git init
+
+# index and stage files
 git add -A
 
 # commit changes
 git commit -m 'Deploy to GitHub Pages.'
 
 # push to gh-pages branch
-git push -f git@github-personal/divisor-client.git main:gh-pages
+git push -f git@github-personal:ilkrklc/divisor-client.git master:gh-pages
 
 # go back to previous working directory
 cd -
