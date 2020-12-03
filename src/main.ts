6fe0323a416f7a5a05f8614a3659fdbc460bff72
İlker Kılıç <ilkrklc@gmail.com>
2020-12-03 22:37:47 +0300

    Integrate google tag manager (#27)
    * Add vue-gtm package to dependencies
    
    * Implement gtm setup

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createGtm } from 'vue-gtm';
 
 import App from './App.vue';
 
@@ -7,5 +8,12 @@ import { store } from './store';
 
 createApp(App)
   .use(store)
+  .use(
+    createGtm({
+      id: 'GTM-5MCHXN5',
+      vueRouter: router,
+      enabled: process.env.NODE_ENV === 'production',
+    }),
+  )
   .use(router)
   .mount('#app');
