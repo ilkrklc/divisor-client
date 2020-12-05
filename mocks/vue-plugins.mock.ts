2e7ff7a69af40e0f2d41cbb69db25ff8ed4773a8
İlker Kılıç <ilkrklc@gmail.com>
2020-12-05 22:39:45 +0300

    Add mocks for vue plugins

---
+++ mocks/vue-plugins.mock.ts
@@ -0,0 +1,22 @@
+import { defineComponent } from 'vue';
+import Vuex from 'vuex';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const Component = defineComponent({});
+
+export const store = new Vuex.Store({
+  getters: {
+    getVersion: jest.fn(),
+  },
+});
+
+export const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Component,
+    },
+  ],
+});
