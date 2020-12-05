a474729ef8504605594b8a0dfc3ad39242f20981
İlker Kılıç <ilkrklc@gmail.com>
2020-12-05 17:12:27 +0300

    Add title constants

---
+++ src/helpers/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Base prefix for html title
+ */
+export const TitleBase = 'Divisor';
+
+/**
+ * Separator for base title prefix and page based title parts
+ */
+export const TitleSeparator = '|';
