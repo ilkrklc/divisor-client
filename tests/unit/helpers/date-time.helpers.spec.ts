446d1aaceea55476d5032f1b24c4435274b44b86
İlker Kılıç <ilkrklc@gmail.com>
2020-12-05 20:15:55 +0300

    Remove main describe from date time helper tests

--- tests/unit/helpers/date-time.helpers.spec.ts
+++ tests/unit/helpers/date-time.helpers.spec.ts
@@ -4,45 +4,43 @@ import {
   getFormattedDateString,
 } from '@/helpers/date-time.helpers';
 
-describe('date time helpers', () => {
-  describe('getUserLocale', () => {
-    const navigatorLanguagesSpy = jest.spyOn(navigator, 'languages', 'get');
-    const navigatorLanguageSpy = jest.spyOn(navigator, 'language', 'get');
+describe('getUserLocale', () => {
+  const navigatorLanguagesSpy = jest.spyOn(navigator, 'languages', 'get');
+  const navigatorLanguageSpy = jest.spyOn(navigator, 'language', 'get');
 
-    let date = new Date();
+  let date = new Date();
 
-    beforeEach(() => {
-      navigatorLanguagesSpy.mockClear();
-      navigatorLanguageSpy.mockClear();
+  beforeEach(() => {
+    navigatorLanguagesSpy.mockClear();
+    navigatorLanguageSpy.mockClear();
 
-      date = new Date();
-    });
+    date = new Date();
+  });
 
-    it('should return default locale when navigator languages empty', () => {
-      navigatorLanguagesSpy.mockImplementation(() => []);
-      navigatorLanguageSpy.mockImplementation(() => '');
+  it('should return default locale when navigator languages empty', () => {
+    navigatorLanguagesSpy.mockImplementation(() => []);
+    navigatorLanguageSpy.mockImplementation(() => '');
 
-      expect(getUserLocale()).toBe(DefaultLocale);
-    });
+    expect(getUserLocale()).toBe(DefaultLocale);
+  });
 
-    it('should return corresponding locale date string', () => {
-      navigatorLanguagesSpy.mockImplementation(() => ['en-US']);
-      navigatorLanguageSpy.mockImplementation(() => 'en-US');
-      expect(getFormattedDateString(date)).toBe(date.toLocaleString('en-US'));
+  it('should return corresponding locale date string', () => {
+    navigatorLanguagesSpy.mockImplementation(() => ['en-US']);
+    navigatorLanguageSpy.mockImplementation(() => 'en-US');
+    expect(getFormattedDateString(date)).toBe(date.toLocaleString('en-US'));
 
-      navigatorLanguagesSpy.mockClear();
-      navigatorLanguageSpy.mockClear();
+    navigatorLanguagesSpy.mockClear();
+    navigatorLanguageSpy.mockClear();
 
-      navigatorLanguagesSpy.mockImplementation(() => ['tr-TR']);
-      navigatorLanguageSpy.mockImplementation(() => 'tr-TR');
-      expect(getFormattedDateString(date)).toBe(date.toLocaleString('tr-TR'));
+    navigatorLanguagesSpy.mockImplementation(() => ['tr-TR']);
+    navigatorLanguageSpy.mockImplementation(() => 'tr-TR');
+    expect(getFormattedDateString(date)).toBe(date.toLocaleString('tr-TR'));
 
-      navigatorLanguagesSpy.mockClear();
-      navigatorLanguageSpy.mockClear();
+    navigatorLanguagesSpy.mockClear();
+    navigatorLanguageSpy.mockClear();
 
-      navigatorLanguagesSpy.mockImplementation(() => ['es-ES']);
-      navigatorLanguageSpy.mockImplementation(() => 'es-ES');
-      expect(getFormattedDateString(date)).toBe(date.toLocaleString('es-ES'));
-    });
+    navigatorLanguagesSpy.mockImplementation(() => ['es-ES']);
+    navigatorLanguageSpy.mockImplementation(() => 'es-ES');
+    expect(getFormattedDateString(date)).toBe(date.toLocaleString('es-ES'));
   });
 });
