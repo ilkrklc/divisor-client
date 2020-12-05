af7fc68f0c34019b66a0c45c903878c4b1775236
İlker Kılıç <ilkrklc@gmail.com>
2020-12-05 17:25:30 +0300

    Add page specific document title support (#36)
    * Add title constants
    
    * Add title getter seo helper
    
    * Add router navigation guard for dynamic titles
    
    * Update webpack html title config with base prefix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,46 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
+import { prepareTitle } from '@/helpers/seo-helpers';
+
 import Home from '@/views/Home.vue';
 
+/**
+ * App routes
+ */
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: prepareTitle('Calculator'),
+    },
   },
   {
     path: '/about',
     name: 'About',
     component: () =>
       import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+    meta: {
+      title: prepareTitle('About'),
+    },
   },
 ];
 
+/**
+ * Router instance
+ */
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+/**
+ * Router navigation guard handles document title
+ */
+router.afterEach(to => {
+  // set document title
+  document.title = to.meta.title;
 });
 
 export default router;
