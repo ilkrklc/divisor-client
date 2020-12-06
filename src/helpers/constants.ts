46fc7769650a9a201fbd8413dc8e6ce9e16002f6
İlker Kılıç <ilkrklc@gmail.com>
2020-12-06 22:17:00 +0300

    Externalize infinite scroll related constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -12,3 +12,15 @@ export const TitleSeparator = '|';
  * Default app locale to be used when navigator languages empty
  */
 export const DefaultLocale = 'en-US';
+
+/**
+ * Scroll offset for infinite scroll effect
+ * @summary Offset from page bottom in pixels to trigger loading new items
+ */
+export const INFINITE_SCROLL_OFFSET = 300;
+
+/**
+ * Item count to load on every load event trigger
+ * @summary Indicates least items can be shown as paged
+ */
+export const INFINITE_SCROLL_PAGE_SIZE = 5;
