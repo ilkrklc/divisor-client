40a0beae2c0de43156b1d56d3337734b068179cd
İlker Kılıç <ilkrklc@gmail.com>
2020-12-06 23:59:20 +0300

    Refactor root mutations unit tests

--- tests/unit/store/modules/root/root.mutations.spec.ts
+++ tests/unit/store/modules/root/root.mutations.spec.ts
@@ -2,16 +2,12 @@ import {
   rootMutations,
   RootMutationType,
 } from '@/store/modules/root/root.mutations';
-import { RootState } from '@/store/modules/root/root.state';
+import { rootState } from '@/store/modules/root/root.state';
 
 describe('root mutations', () => {
   it(RootMutationType.ChangeVersion, () => {
-    const state: RootState = {
-      version: '0.0.0',
-    };
+    rootMutations.CHANGE_VERSION(rootState, '11.0.0');
 
-    console.log(rootMutations.CHANGE_VERSION(state, '1.0.0'));
-
-    expect(state.version).toBe('1.0.0');
+    expect(rootState.version).toBe('11.0.0');
   });
 });
