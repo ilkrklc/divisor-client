bddb82b5c61756ca846a5bc8308355e2c9183a0e
Kutay Batı <kutaybati@gmail.com>
2020-12-12 12:25:56 +0300

    height changed

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -96,7 +96,7 @@ export default defineComponent({
   .calculator-navigation {
     @include flex(row);
 
-    height: 3.5rem;
+    height: 3.51rem;
 
     .calculator-tab-item {
       @include font-style(1rem, 600, 0.5, $color-medium);
