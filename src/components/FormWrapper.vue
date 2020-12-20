eca08605544ffb87a6494c058668da552029fea6
Kutay Batı <kutaybati@gmail.com>
2020-12-20 16:25:39 +0300

    Set color to input & select

--- src/components/FormWrapper.vue
+++ src/components/FormWrapper.vue
@@ -44,28 +44,37 @@ export default defineComponent({
   input[type='text'] {
     @include padding-x(0.75rem);
     @include padding-y(0.5rem);
+    @include font-style(1.1rem, 400, 1, $color-text);
+    @include shadow-centered(2px, 0.25);
 
     font-family: $font-raj;
     height: 2.5rem;
-    border: 1px solid rgba(0, 0, 0, 0.6);
+    border: 1px solid $color-border;
   }
-
-  button[type='submit'] {
-    @include font-style(1.2rem, 600, 1, $color-dark);
-    @include padding-x(4rem);
-    @include padding-y(0.75rem);
-
-    background: transparent;
-    border: 1px solid $color-dark;
+  input[type='text']:focus,
+  select:focus {
+    box-shadow: 0 0 3px $color-green;
+    outline-color: $color-green;
   }
-
   select {
     @include padding-x(0.75rem);
     @include padding-y(0.25rem);
+    @include font-style(1.1rem, 400, 1, $color-text);
+    @include shadow-centered(2px, 0.25);
 
     font-family: $font-raj;
     flex-grow: 1;
     height: 2.5rem;
+    border: 1px solid $color-border;
+  }
+
+  button[type='submit'] {
+    @include font-style(1.2rem, 600, 1, $color-text);
+    @include padding-x(4rem);
+    @include padding-y(0.75rem);
+
+    background: transparent;
+    border: 1px solid $color-text;
   }
 
   .form-label {
